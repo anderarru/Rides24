e5290779baed780a72a1d3db413a680dbb6af41f
saizx <saizx@192.168.1.139>
2024-10-06 14:57:49 +0200

    intentionality critical issue

--- src/main/java/businesslogic/BusinessLogicServer.java
+++ src/main/java/businesslogic/BusinessLogicServer.java
@@ -22,7 +22,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
+import javax.swing.WindowConstants;
 /**
  * It runs the business logic server as a separate process.
  */
@@ -40,7 +40,7 @@ public class BusinessLogicServer extends JDialog {
 	public static void main(String[] args) {
 		try {
 			BusinessLogicServer dialog = new BusinessLogicServer();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
