e98e0d3b562a073d0e9d61656bd3647b39628520
anderarru <aarruti031@ikasle.ehu.eus.com>
2024-11-07 23:01:00 +0100

    adapter finished (factory needed)

--- src/main/java/adapter/AdapterMain.java
+++ src/main/java/adapter/AdapterMain.java
@@ -14,9 +14,9 @@ public class AdapterMain {
 	 public static void main(String[] args) {
 		 //	the	BL	is	local
 		 boolean isLocal =	true;
-		 BLFacade blFacade =  new BLFactory().getBusinessLogicFactory(isLocal);
-		 Driver	d= blFacade.getDriver("Urtzi");
-		 DriverTable dt=new DriverTable(d);
-		 dt.setVisible(true);
+		 //BLFacade blFacade =  new BLFactory().getBusinessLogicFactory(isLocal);
+		 //Driver	d= blFacade.getDriver("Urtzi");
+		 //DriverTable dt=new DriverTable(d);
+		 //dt.setVisible(true);
 	 }
 }
